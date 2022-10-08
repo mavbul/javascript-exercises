66189d246f38f80d931428287513020e2713b973
mavbul <mavbul@mail.ru>
2022-10-08 17:07:12 +0300

    sol

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let finalStr = '';
+    if (num < 0) return 'ERROR';
+    while (num > 0) {
+        finalStr += str;
+        num--;
+    }
+    return finalStr;
 };
 
 // Do not edit below this line
