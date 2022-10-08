910ddaf36ec6aef71efccfd261c1680a0c09ad33
mavbul <mavbul@mail.ru>
2022-10-08 20:25:04 +0300

    3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let returnStr = '';
+    let index = str.length - 1;
+    while (index >= 0 ) {
+        returnStr += str[index];
+        index--; 
+    }
+    return returnStr;
 };
 
 // Do not edit below this line
