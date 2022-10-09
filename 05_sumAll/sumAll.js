d981ac4bc9a47a62e295cb88ca6b1457c0987f3a
mavbul <mavbul@mail.ru>
2022-10-09 20:31:42 +0300

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (typeof(first) != 'number' || typeof(second) != 'number') return 'ERROR';
+    if (first < 0 || second < 0) return 'ERROR';
+    let buffer;
+    if (first > second) {
+        buffer = first;
+        first = second;
+        second = buffer;
+    }
+    let sum = 0;
+    for (let i = first; i <= second; i++) {
+        sum += i; 
+    }
+
+    return sum;
 
 };
 
