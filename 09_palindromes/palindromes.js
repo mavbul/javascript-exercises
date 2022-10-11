7d8ba82b184b545ca7293803f510273bcff228d3
mavbul <mavbul@mail.ru>
2022-10-11 21:50:55 +0300

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let inputArray = Array.from(str);
+    inputArray = removeFromArray(inputArray, '.', ',', ' ','!');
+
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] !== inputArray[inputArray.length - 1 - i]) return false;
+    }
+    return true; 
+};
+
+const removeFromArray = function(initialArray, ...args) {
+    let finalArray = [];
+    for (let itemInitialArray of initialArray) {
+        if (!args.includes(itemInitialArray)) finalArray.push(itemInitialArray);
+    } 
+    return finalArray;
 
 };
 
