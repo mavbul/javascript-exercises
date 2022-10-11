18b70193853f33710c56eb80d6b22478f232f3af
mavbul <mavbul@mail.ru>
2022-10-11 18:22:42 +0300

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(...args) {
+	let sum = 0;
+  for (let value of args) {
+    sum += value;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return +(+a - +b);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let value of array) {
+    sum += value;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let value of array) {
+    product *= value;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(baze, power) {
+  let result = 1; 
+  for (i = 1; i <= power; i++) {
+    result *= baze; 
+  }
+  return result; 
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let result = 1;
+  for (let i = 1; i <= fact; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
