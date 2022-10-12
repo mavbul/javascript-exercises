7113df419949cbf5191a61c85211efd9fe1b1c35
mavbul <mavbul@mail.ru>
2022-10-12 21:25:16 +0300

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(fibo) {
+    if (fibo <= 0) return 'OOPS';
 
+    let fibonacciArray = [1, 1];
+
+    for (let i = 2; i < fibo; i++) {
+        fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2]; 
+    }
+
+    return fibonacciArray[fibo-1];
 };
 
 // Do not edit below this line
