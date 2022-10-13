5ba17d4bf8c6d60a3a6513c2d6577d5dc5924320
mavbul <mavbul@mail.ru>
2022-10-13 21:09:49 +0300

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayPeopleObj) {
+    let oldestIndex = 0;
+    let howOldBuffer = 0; 
+    for (let i = 0; i < arrayPeopleObj.length; i++) {
+        const now = new Date();
+        if (!arrayPeopleObj[i].yearOfDeath) arrayPeopleObj[i].yearOfDeath = now.getFullYear();
+        const howOld = arrayPeopleObj[i].yearOfDeath - arrayPeopleObj[i].yearOfBirth;
+        console.log(howOld);
+        console.log(howOldBuffer);
+        if (howOld > howOldBuffer) {
+            oldestIndex = i;
+            howOldBuffer = howOld;
+        }
+        console.log(oldestIndex);
+    }
 
+    return arrayPeopleObj[oldestIndex];
 };
 
 // Do not edit below this line
